540c5eeacadf7718cc8f081caa280bd0266d1486
Michael Banvard <mjbanvard@gmail.com>
2019-10-16 15:53:16 -0500

    First draft thorugh to Part 3

--- exercises/part1-2.js
+++ exercises/part1-2.js
@@ -1,23 +1,18 @@
 // URL for the instructions: 
 // https://education.launchcode.org/intro-to-professional-web-dev/chapters/typescript/exercises.html 
-
 // Part 1: Add the 5 variables here
-let spacecraftName: string = 'Determination';
-let speedMph: number = 17500;
-let kilometersToMars: number = 225000000;
-let kilometersToTheMoon: number = 384400;
-let milesPerKilometer: number = 0.621;
-
+var spacecraftName = 'Determination';
+var speedMph = 17500;
+var kilometersToMars = 225000000;
+var kilometersToTheMoon = 384400;
+var milesPerKilometer = 0.621;
 // Code part 2 here:
 // milesToMars is a number with the value of kilometersToMars * milesPerKilometer.
 // hoursToMars is a number with the value of milesToMars / speedMph.
 // daysToMars is a number with the value of hoursToMars / 24.
-let milesToMars: number = kilometersToMars * milesPerKilometer;
-let hoursToMars: number = milesToMars / speedMph;
-let daysToMars: number = hoursToMars / 24;
-
+var milesToMars = kilometersToMars * milesPerKilometer;
+var hoursToMars = milesToMars / speedMph;
+var daysToMars = hoursToMars / 24;
 // Code the output statement here (use a template literal):
-
 // Determination would take 332.67857142857144 days to get to Mars.
-console.log(`${spacecraftName} would take ${daysToMars} days to get to Mars.`);
-
+console.log(spacecraftName + " would take " + daysToMars + " days to get to Mars.");
--- exercises/part3.js
+++ exercises/part3.js
@@ -1,12 +1,10 @@
 // URL for the instructions: 
 // https://education.launchcode.org/intro-to-professional-web-dev/chapters/typescript/exercises.html 
-
-let spacecraftName: string = "Deteremination";
-let speedMph: number = 17500;
-let kilometersToMars: number = 225000000;
-let kilometersToTheMoon: number = 384400;
-let milesPerKilometer: number = 0.621;
-
+var spacecraftName = "Deteremination";
+var speedMph = 17500;
+var kilometersToMars = 225000000;
+var kilometersToTheMoon = 384400;
+var milesPerKilometer = 0.621;
 // Code the "getDaysToLocation" function here:
 // In the space indicated, define a function that calculates the days it would take 
 //      to travel.
@@ -24,16 +22,15 @@ let milesPerKilometer: number = 0.621;
 //  Print out the days to the Moon.
 //  Use template literals, ${getDaysToLocation(kilometersToTheMoon)} and 
 //      ${spacecraftName}.
-function getDaysToLocation(kilometersAway: number): number {
-    let milesAway: number = kilometersAway * milesPerKilometer;
-    let hours: number = milesAway / speedMph;
-    let daysAway: number = hours / 24;
-    return daysAway
-};
-
+function getDaysToLocation(kilometersAway) {
+    var milesAway = kilometersAway * milesPerKilometer;
+    var hours = milesAway / speedMph;
+    var daysAway = hours / 24;
+    return daysAway;
+}
+;
 // Call the function and print the outputs for the Mars trip and the moon trip:
-
 // Determination would take 332.67857142857144 days to get to Mars.
 // Determination would take 0.5683628571428571 days to get to the Moon.
-console.log(`${spacecraftName} would take ${getDaysToLocation(kilometersToMars)} days to get to Mars.`);
-console.log(`${spacecraftName} would take ${getDaysToLocation(kilometersToTheMoon)} days to get the Moon.`)
+console.log(spacecraftName + " would take " + getDaysToLocation(kilometersToMars) + " days to get to Mars.");
+console.log(spacecraftName + " would take " + getDaysToLocation(kilometersToTheMoon) + " days to get the Moon.");
